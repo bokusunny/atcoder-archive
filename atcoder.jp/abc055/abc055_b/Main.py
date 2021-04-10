ecb2e172fe46621df07003d5e3017366de382da1
bokusunny <abovillage1407@gmail.com>
2021-04-11 01:31:13 +0900

    [AC] abc055 abc055_b

---
+++ atcoder.jp/abc055/abc055_b/Main.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int, input().split())
+def LIIS(): return list(map(int, input().split()))
+def ZER(N): return [False for _ in range(N)]
+
+
+INF = 10**30
+MOD = 10**9+7
+# ---------------------------------------------------
+N = II()
+p = 1
+for i in range(N):
+  p *= (i+1)
+  p %= MOD
+
+print(p)
