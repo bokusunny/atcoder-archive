64a37a81d844afe198480dba394893fc6bc72700
bokusunny <abovillage1407@gmail.com>
2021-04-11 01:36:16 +0900

    [AC] abc046 abc046_b

---
+++ atcoder.jp/abc046/abc046_b/Main.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int, input().split())
+def LIIS(): return list(map(int, input().split()))
+def ZER(N): return [False for _ in range(N)]
+
+
+INF = 10**30
+MOD = 10**9+7
+# ---------------------------------------------------
+N, K = IIS()
+paterns = K
+for _ in range(N-1):
+  paterns *= (K-1)
+
+print(paterns)
