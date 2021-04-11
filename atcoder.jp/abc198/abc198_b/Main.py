1ae6c3a554f5c04ae02f26ed30d956c8621b3be7
bokusunny <abovillage1407@gmail.com>
2021-04-11 21:07:19 +0900

    [AC] abc198 abc198_b

---
+++ atcoder.jp/abc198/abc198_b/Main.py
@@ -0,0 +1,21 @@
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int, input().split())
+def IIR(): return [int(input()) for _ in range(N)]
+def LIIS(): return list(map(int, input().split()))
+def ZER(N): return [False for _ in range(N)]
+
+
+INF = 10**30
+MOD = 10**9+7
+# ---------------------------------------------------
+N = I()
+for i in range(len(N)):
+  if N == N[::-1]:
+    print('Yes')
+    exit()
+
+  N = '0' + N
+
+print('No')
