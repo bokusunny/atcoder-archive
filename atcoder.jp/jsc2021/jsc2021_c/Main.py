ac5f1ec5d38fc1044c506b2cd20ecf6fa9cbcf00
bokusunny <abovillage1407@gmail.com>
2021-04-17 23:21:49 +0900

    [AC] jsc2021 jsc2021_c

---
+++ atcoder.jp/jsc2021/jsc2021_c/Main.py
@@ -0,0 +1,17 @@
+def I(): return input()
+def IS(): return input().split()
+def II(): return int(input())
+def IIS(): return map(int, input().split())
+def IIR(): return [int(input()) for _ in range(N)]
+def LIIS(): return list(map(int, input().split()))
+def ZER(N): return [False for _ in range(N)]
+
+
+INF = 10**30
+MOD = 10**9+7
+# ---------------------------------------------------
+A, B = IIS()
+
+for c in range(B, 0, -1):
+	if B//c - (A-1)//c >= 2:
+		exit(print(c))
