d976ee2760616f079bf0594eee7e69652b16cece
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2021-04-17 23:24:07 +0900

    Add jsc2021 review

---
+++ test_run.py
@@ -0,0 +1,18 @@
+# debug here :) Good atcoder life!
+# ref: https://github.com/YujiSoftware/ac-unit-test
+def resolve():
+	# import sys
+	# input = sys.stdin.readline
+	def I(): return input()
+	def IS(): return input().split()
+	def II(): return int(input())
+	def IIS(): return map(int, input().split())
+	def IIR(): return [int(input()) for _ in range(N)]
+	def LIIS(): return list(map(int, input().split()))
+	def ZER(N): return [False for _ in range(N)]
+
+
+	INF = 10**30
+	MOD = 10**9+7
+	# ---------------------------------------------------
+# ---------------------------------------------------
