4cbcfb9b824bd5b7ca9c8d97d264bd1c29f48603
bokusunny <abovillage1407@gmail.com>
2021-05-11 22:00:14 +0900

    [AC] sumitrust2019 sumitb2019_a

---
+++ atcoder.jp/sumitrust2019/sumitb2019_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _GLIBCXX_DEBUG
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+// --------------------------------------------------------
+int main() {
+  int m1,d1,m2,d2;
+  cin>>m1>>d1>>m2>>d2;
+
+  cout << !(d2-d1 == 1) << endl;
+}
