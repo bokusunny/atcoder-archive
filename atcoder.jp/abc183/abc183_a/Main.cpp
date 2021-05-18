515c1ba1421411ae660eac80bbb1f02855b466d5
bokusunny <abovillage1407@gmail.com>
2021-05-18 11:21:59 +0900

    [AC] abc183 abc183_a

---
+++ atcoder.jp/abc183/abc183_a/Main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _GLIBCXX_DEBUG
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+
+template <typename T> inline bool chmin(T &a, const T &b) {
+  bool compare = a > b;
+  if (a > b)
+    a = b;
+  return compare;
+}
+template <typename T> inline bool chmax(T &a, const T &b) {
+  bool compare = a < b;
+  if (a < b)
+    a = b;
+  return compare;
+}
+// --------------------------------------------------------
+int main() {
+  int x;
+  cin >> x;
+  if (x >= 0) {
+    cout << x << endl;
+  } else {
+    cout << 0 << endl;
+  }
+}
