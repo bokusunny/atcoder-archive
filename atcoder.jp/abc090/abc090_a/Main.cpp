fc981bcabdd86e94a7e90e092ee5b2f7d54fae3d
bokusunny <abovillage1407@gmail.com>
2021-06-01 15:33:59 +0900

    [AC] abc090 abc090_a

---
+++ atcoder.jp/abc090/abc090_a/Main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s1, s2, s3;
+  cin >> s1 >> s2 >> s3;
+  string ans;
+  ans = ans + s1[0] + s2[1] + s3[2];
+
+  cout << ans << endl;
+
+  return 0;
+}
