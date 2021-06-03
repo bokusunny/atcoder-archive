f4925bb8e5ac1c121d51eeeddfefb38a908e7afa
bokusunny <abovillage1407@gmail.com>
2021-06-03 23:31:58 +0900

    [AC] arc030 arc030_1

---
+++ atcoder.jp/arc030/arc030_1/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  
+  if (k<=n/2) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+
+  return 0;
+}
