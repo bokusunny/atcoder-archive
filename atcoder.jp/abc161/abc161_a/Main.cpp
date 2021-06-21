344a64636cd2c6d5c7cf958cdaa049185faa1116
bokusunny <abovillage1407@gmail.com>
2021-06-21 22:21:12 +0900

    [AC] abc161 abc161_a

---
+++ atcoder.jp/abc161/abc161_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << c << " ";
+  cout << a << " ";
+  cout << b << endl;
+
+  return 0;
+}
