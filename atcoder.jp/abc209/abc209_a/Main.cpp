6f4a35476c56deeaf1f78315f6e6a5dd3ca1dd96
bokusunny <abovillage1407@gmail.com>
2021-07-10 21:12:18 +0900

    [AC] abc209 abc209_a

---
+++ atcoder.jp/abc209/abc209_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+
+  if (A > B) {
+    cout << 0 << endl;
+  } else {
+    cout << B - A + 1 << endl;
+  }
+
+  return 0;
+}
