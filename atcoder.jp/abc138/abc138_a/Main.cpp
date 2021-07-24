1340026689538f5656fe4e646654e6201361d298
bokusunny <abovillage1407@gmail.com>
2021-07-24 09:20:51 +0900

    [AC] abc138 abc138_a

---
+++ atcoder.jp/abc138/abc138_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  string S;
+  cin >> S;
+  if (N >= 3200)
+    cout << S << endl;
+  else
+    cout << "red" << endl;
+
+  return 0;
+}
