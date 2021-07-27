bcc6b4c274f127e31b1405474d716d6861027c71
bokusunny <abovillage1407@gmail.com>
2021-07-27 16:36:28 +0900

    [AC] abc167 abc167_e

---
+++ atcoder.jp/abc167/abc167_e/Main.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <atcoder/modint>
+using namespace atcoder;
+
+using mint = modint998244353;
+
+long long N, M, K;
+const long long MOD = 998244353;
+
+struct ModComb {
+ private:
+  vector<long long> Fac, Finv, Inv;
+  long long MOD;
+  int MAX_N;
+
+  bool is_prime(long long x) {
+    if (x == 1) return false;
+
+    long long i = 2;
+    while (i * i <= x) {
+      if (x % i == 0) return false;
+      i++;
+    }
+    return true;
+  }
+
+ public:
+  ModComb(long long mod = 1e9 + 7, int max_n = 1 << 20) {
+    assert(is_prime(mod) && mod > max_n);
+
+    MOD = mod, MAX_N = max_n;
+    Fac.resize(MAX_N + 1), Finv.resize(MAX_N + 1), Inv.resize(MAX_N + 1);
+    Fac[0] = Fac[1] = 1;
+    Finv[0] = Finv[1] = 1;
+    Inv[1] = 1;
+
+    for (int i = 2; i <= MAX_N; i++) {
+      Fac[i] = Fac[i - 1] * i % MOD;
+      Inv[i] = MOD - Inv[MOD % i] * (MOD / i) % MOD;
+      Finv[i] = Finv[i - 1] * Inv[i] % MOD;
+    }
+  }
+
+  long long get(int n, int k) {
+    assert(n <= MAX_N);
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+
+    return Fac[n] * (Finv[k] * Finv[n - k] % MOD) % MOD;
+  }
+};
+
+mint modpow(mint a, long long n) {
+  mint res = 1;
+  while (n > 0) {
+    if (n & 1) res = res * a;
+    a = a * a;
+    n >>= 1;
+  }
+  return res;
+}
+
+int main() {
+  cin >> N >> M >> K;
+
+  ModComb Comb(MOD, N);
+
+  mint ans = 0;
+  for (int k = 0; k <= K; k++) {
+    mint tmp = M;
+    tmp *= modpow(M - 1, N - k - 1);
+    tmp *= Comb.get(N - 1, k);
+    ans += tmp;
+  }
+
+  cout << ans.val() << endl;
+
+  return 0;
+}
