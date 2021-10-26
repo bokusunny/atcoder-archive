32e7cef77ab79cd83a64b1ac4e3962d2306c1f20
bokusunny <abovillage1407@gmail.com>
2021-10-26 15:34:43 +0900

    [AC] abc224 abc224_f

---
+++ atcoder.jp/abc224/abc224_f/Main.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <int mod>
+struct ModInt {
+  int x;
+
+  ModInt() : x(0) {}
+
+  ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+  ModInt &operator+=(const ModInt &p) {
+    if ((x += p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator-=(const ModInt &p) {
+    if ((x += mod - p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator*=(const ModInt &p) {
+    x = (int)(1LL * x * p.x % mod);
+    return *this;
+  }
+
+  ModInt &operator/=(const ModInt &p) {
+    *this *= p.inverse();
+    return *this;
+  }
+
+  ModInt operator-() const { return ModInt(-x); }
+
+  ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+  ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+  ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+  ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+  bool operator==(const ModInt &p) const { return x == p.x; }
+
+  bool operator!=(const ModInt &p) const { return x != p.x; }
+
+  ModInt inverse() const {
+    int a = x, b = mod, u = 1, v = 0, t;
+    while (b > 0) {
+      t = a / b;
+      swap(a -= t * b, b);
+      swap(u -= t * v, v);
+    }
+    return ModInt(u);
+  }
+
+  ModInt pow(int64_t n) const {
+    ModInt ret(1), mul(x);
+    while (n > 0) {
+      if (n & 1) ret *= mul;
+      mul *= mul;
+      n >>= 1;
+    }
+    return ret;
+  }
+
+  friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+
+  friend istream &operator>>(istream &is, ModInt &a) {
+    int64_t t;
+    is >> t;
+    a = ModInt<mod>(t);
+    return (is);
+  }
+
+  static int get_mod() { return mod; }
+};
+
+const int MOD = 998244353;
+using mint = ModInt<MOD>;
+
+void solve() {
+  string S;
+  cin >> S;
+  int N = (int)S.size();
+
+  mint ans = 0;
+  for (int x = 0; x < N; x++) {
+    mint tmp = 1;
+    tmp *= ((mint)2).pow(x);
+
+    int y = N - x - 1;
+    if (y == 0) {
+      tmp *= (S[x] - '0');
+    } else {
+      tmp *= (((mint)2).pow(y - 1) * (((mint)5).pow(y) - 1) / 4 + ((mint)10).pow(y)) * (S[x] - '0');
+    }
+    ans += tmp;
+  }
+
+  cout << ans << endl;
+}
+
+void setcin() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+}
+
+int main() {
+  setcin();
+  solve();
+
+  return 0;
+}
